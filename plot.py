6972fb16c36efaf14f250c64b1126422fdc59da1
Kal Hourani <khalid.hourani@gmail.com>
2020-04-21 05:34:27 -0500

    Logic for averaging simulation runs

--- plot.py
+++ plot.py
@@ -5,29 +5,31 @@ import numpy as np
 import csv
 
 
-def plot_growth_rate(n, p, number_of_new_cases, plot):
+def plot_average_growth_rate(n, p, number_of_new_cases):
     number_of_days = len(number_of_new_cases)
 
     fig = plt.figure()
     plot = fig.add_subplot(1, 1, 1)
     plot.set_xlabel("Day")
     plot.set_ylabel("Number of New Cases")
-    plot.set_title(f"Number of New Cases with n = {n}, T_p = {p:0.2f}")
+    plot.set_title(
+        f"Number of New Cases with n = {n}, T_p = {p:0.2f}\n(Average over 10 Simulations)"
+    )
 
     X = np.array(range(1, number_of_days + 1))
     Y = np.array(number_of_new_cases)
     plt.scatter(X, Y)
-    plt.savefig(f"./output_files/plot_{n}_{p:0.2f}.png")
+    plt.savefig(f"./output_files/plots/average_plot_{n}_{p:0.2f}.png")
 
 
 def main():
-    with open("./output_files/growth_data_1000000.csv") as data_file:
+    with open("./output_files/csvs/average_growth_data_1000000.csv") as data_file:
         rows = csv.reader(data_file)
         next(rows)
         for row in rows:
             n, p = int(row[0]), float(row[1])
-            data = [int(x) for x in row[2:]]
-            plot_growth_rate(n, p, data)
+            data = [float(x) for x in row[2:]]
+            plot_average_growth_rate(n, p, data)
 
 
 if __name__ == "__main__":
